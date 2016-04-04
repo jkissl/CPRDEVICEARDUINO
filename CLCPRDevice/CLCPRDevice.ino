4680e53aaed70dbbc5ca8adff7e37b24ef92a9e1
jkissl <josephkissling3@gmail.com>
2016-04-04 15:09:14 -0400

    Comments added.
    Comments to clarify the function of the if statements when reading the
    packets.

--- CLCPRDevice/CLCPRDevice.ino
+++ CLCPRDevice/CLCPRDevice.ino
@@ -1,9 +1,10 @@
 /*
   Name:		CLCPRDevice.ino
   Created:	3/30/2016 6:50:13 PM
-  Author:	Mitchell Baldwin & Joseph Kissling 
-  Quick check. Comments comming soon
+  Author:	Mitchell Baldwin & Joseph Kissling
 */
+
+// Libraries for functions
 #include <PacketSerial.h>
 #include <EEPROM.h>
 
@@ -15,12 +16,18 @@ uint8_t outPacket[PACKET_SIZE];
 // Software Verson info
 byte MajorFVN_Verson = 1;
 byte MinorFVN_Verson = 21;
+
+// Software Versone packet
 byte FVNPkt[2];
 
 // Hardware Verson info
 byte MajorHVN_Verson = 1;
 byte MinorHVN_Verson = 5;
+
+// Hardware packet
 byte HVNPkt[2];
+
+// EEPROM addresses
 int MAJORrev_EEPROM_addr = 0;
 int MINORrev_EEPROM_addr = 1;
 
@@ -32,15 +39,21 @@ PacketSerial spUSB;
 
 void setup()
 {
+  // Set the pin mode
   pinMode(LEDPIN, OUTPUT);
+
+  // Build the Firmware packet
   FVNPkt[0] = MajorFVN_Verson;
   FVNPkt[1] = MinorFVN_Verson;
 
+  // Build the major communication packets
   for (int i = 0; i < PACKET_SIZE; ++i)
   {
     inPacket[i] = 0x00;
     outPacket[i] = 0x00;
   }
+
+  // Turn on the packe communication system
   spUSB.setPacketHandler(&OnUSBPacket);
   spUSB.begin(115200);
 }
@@ -51,24 +64,19 @@ void loop()
   delay(10);
 }
 
+// Read and respond to the packets
 void OnUSBPacket(const uint8_t* buffer, size_t size)
 {
-  if (buffer[0] == 0x10)				// Test case
+  // Test case for returing the stated of the device
+  if (buffer[0] == 0x10)
   {
     ToggleUserLED();
     outPacket[0] = 255;
-    outPacket[1] = 255;
-    outPacket[2] = 255;
-    outPacket[3] = 255;
-    outPacket[4] = 255;
-    outPacket[5] = 255;
-    outPacket[6] = 255;
-    outPacket[7] = 255;
     SendPacket();
-
-
   }
-  if (buffer[0] == 0xA3)        //testparm
+
+  // send the firmware version
+  if (buffer[0] == 0xA3)
   {
     ToggleUserLED();
     SendFVN();
@@ -82,7 +90,7 @@ void OnUSBPacket(const uint8_t* buffer, size_t size)
     SendPacket();
   }
 
-  // Set hardware verson
+  // Set hardware verson, writes the values to EEPROM
   if (buffer[0] == 0xA5)
   {
     // MajorHVN_Verson = buffer[1];
@@ -90,12 +98,14 @@ void OnUSBPacket(const uint8_t* buffer, size_t size)
     EEPROM.write(MAJORrev_EEPROM_addr, MajorHVN_Verson);
     EEPROM.write(MINORrev_EEPROM_addr, MinorHVN_Verson);
   }
-if (buffer[0] == 0xA5)
-{
-  // CPR Phase times
-}
 
-  // State Flag reader
+  // Set CPR Phase times
+  if (buffer[0] == 0x20)
+  {
+    // CPR Phase times
+  }
+
+  // State Flag reader for indicating & controlling the state of the subsystems
   for (int j = 0; j < 4; ++j)
   {
     if (buffer[0] & _BV(j))
@@ -120,6 +130,7 @@ if (buffer[0] == 0xA5)
   }
 }
 
+// Switches toggles the LED, used as an heartbeat to verifiy commmunication
 void ToggleUserLED()
 {
   if (digitalRead(LEDPIN) == HIGH)
@@ -132,6 +143,7 @@ void ToggleUserLED()
   }
 }
 
+// Send the data packet to the computer.
 void SendPacket()
 {
   int checksum = 0;
@@ -143,6 +155,7 @@ void SendPacket()
   spUSB.send(outPacket, PACKET_SIZE);
 }
 
+// Send the firmware verson packet
 void SendFVN()
 {
   outPacket[0] = FVNPkt[0];
